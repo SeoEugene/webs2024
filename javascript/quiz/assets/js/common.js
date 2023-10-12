902d67e8632f7266c2eb9609cccb8b5eb09680ba
SeoEugene <97.eugene.s@gmail.com>
2023-10-13 07:32:50 +0900

    23년 10월 12일
    모달

--- javascript/quiz/assets/js/common.js
+++ javascript/quiz/assets/js/common.js
@@ -1,17 +1,30 @@
-// hljs.highlightAll();
+hljs.highlightAll();
 
-// modal
+// 모달
 const modalBtn = document.querySelector(".modal__btn");
 const madalClose = document.querySelector(".modal__close");
 const modalCont = document.querySelector(".modal__cont");
 
-// 소스보기 버튼
 modalBtn.addEventListener("click", () => {
     modalCont.classList.add("show");
     modalCont.classList.remove("hide");
 });
-
-// 소스보기 닫기버튼
 madalClose.addEventListener("click", () => {
     modalCont.classList.add("hide");
+});
+
+// 탭메뉴
+const tabBtn = document.querySelectorAll(".modal__box .tabs > div");
+const tabCont = document.querySelectorAll(".modal__box .cont > div");
+
+tabBtn.forEach((element, index) => {
+    element.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        tabBtn.forEach(li => li.classList.remove("active"));
+        element.classList.add("active");
+
+        tabCont.forEach(div => div.style.display = "none");
+        tabCont[index].style.display = "block";
+    })
 });
